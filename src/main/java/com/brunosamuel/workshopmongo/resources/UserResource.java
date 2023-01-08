629fa4b11b8ac5547bb665f9b5c0255b70b9665c
Yuyake23 <brunosamuel.orizona@gmail.com>
2023-01-08 12:58:48 -0300

    Implementado padrao DTO para a classe User

--- src/main/java/com/brunosamuel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/brunosamuel/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.brunosamuel.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,19 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brunosamuel.workshopmongo.dto.UserDTO;
 import com.brunosamuel.workshopmongo.entities.User;
 import com.brunosamuel.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		return ResponseEntity.ok().body(service.findAll());
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
+		List<UserDTO> listDTO = list.stream().map(o -> new UserDTO(o)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
