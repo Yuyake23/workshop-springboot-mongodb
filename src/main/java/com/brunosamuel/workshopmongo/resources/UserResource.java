29659d3642e62342ffe09e1c25846bb6975847fc
Yuyake23 <brunosamuel.orizona@gmail.com>
2023-01-08 14:26:05 -0300

    obtendo User por id e tratamento de excecao

--- src/main/java/com/brunosamuel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/brunosamuel/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +24,17 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		List<UserDTO> listDTO = list.stream().map(o -> new UserDTO(o)).collect(Collectors.toList());
+		List<UserDTO> listDTO = list.stream().map(UserResource::toDTO).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		return ResponseEntity.ok().body(toDTO(service.findById(id)));
+	}
+
+	private static UserDTO toDTO(User obj) {
+		return new UserDTO(obj);
 	}
 
 }
