aebc3ef566541f6279639a801dec69739d8f79a3
Yuyake23 <brunosamuel.orizona@gmail.com>
2023-01-11 16:05:17 -0300

    consulta com query methods findByTitleContaining

--- src/main/java/com/brunosamuel/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/brunosamuel/workshopmongo/repositories/PostRepository.java
@@ -1,9 +1,13 @@
 package com.brunosamuel.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.brunosamuel.workshopmongo.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/com/brunosamuel/workshopmongo/resources/PostResource.java
+++ src/main/java/com/brunosamuel/workshopmongo/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brunosamuel.workshopmongo.entities.Post;
+import com.brunosamuel.workshopmongo.resources.util.URL;
 import com.brunosamuel.workshopmongo.services.PostService;
 
 @RestController
@@ -27,6 +29,14 @@ public class PostResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		return ResponseEntity.ok().body(service.findById(id));
+	}
+
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitleContaining(
+			@RequestParam(defaultValue = "", value = "text") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = service.findByTitleContaining(text);
+		return ResponseEntity.ok().body(posts);
 	}
 
 }
